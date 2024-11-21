0c6e8dd5b413fc4103c4bb126c59b78d9ab6a037
Acyp9 <142829765+Acyp9@users.noreply.github.com>
2024-11-20 21:20:14 -0500

    Add files via upload

---
+++ glacierv5.js
@@ -0,0 +1,18 @@
+function showProxy() {
+  document.getElementById('english').style.visibility = 'hidden';
+  document.getElementById('proxy').style.visibility = 'visible';
+}
+
+function show2048() {
+  document.getElementById('english').style.visibility = 'hidden';
+  document.getElementById('2048').style.visibility = 'visible';
+}
+
+function showBbs() {
+  document.getElementById('english').style.visibility = 'hidden';
+  document.getElementById('Bbs').style.visibility = 'visible';
+}
+
+function refresh() {
+  location.reload();
+}
